f8165d778143eb5a94971d96012d7c3774301f25
Boris Quiroz <boris@insert-coin.org>
2018-01-01 23:15:46 -0300

    vagrant provision script

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ sudo add-apt-repository ppa:jonathonf/python-3.6 -y
 sudo apt-get update
 sudo apt-get install python3.6 -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
-curl -O https://bootstrap.pypa.io/get-pip.py /tmp/get-pip.py
+curl -s -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
 sudo python /tmp/get-pip.py
