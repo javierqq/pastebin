653d82c4f5a6bbd24e292cae37a3cd5e1009b448
Boris Quiroz <boris@insert-coin.org>
2018-01-01 23:02:41 -0300

    Scriptify the install commands

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
-sudo apt update
+#!/bin/bash
+
 sudo add-apt-repository ppa:jonathonf/python-3.6 -y
 sudo apt-get update
 sudo apt-get install python3.6 -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
-sudo python get-pip.py
+curl -O https://bootstrap.pypa.io/get-pip.py /tmp/get-pip.py
+sudo python /tmp/get-pip.py
