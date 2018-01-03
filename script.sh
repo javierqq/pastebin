e83467d14a76a44a3e96f995a552c009b695a378
Javier Quiroz <Jota@Javiers-MacBook-Air.local>
2018-01-03 13:20:47 -0300

    install flask

--- script.sh
+++ script.sh
@@ -6,3 +6,9 @@ sudo apt-get install python3.6 -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
 curl -s -o /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py
 sudo python /tmp/get-pip.py
+sudo pip install virtualenv
+mkdir project
+cd project
+virtualenv venv
+. venv/bin/activate
+pip install Flask
